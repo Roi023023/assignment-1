211707344a65ea4d75911b5c8534bc050072eedc
Roi023023 <137299805+Roi023023@users.noreply.github.com>
2024-05-17 19:30:36 +0300

    fix things with the getWords method

--- test/test/Board.java
+++ test/test/Board.java
@@ -7,6 +7,7 @@ public class Board {
     private static Board board = null;
     private Tile[][] tiles;
     boolean isStarUsed = true;
+    
 
     private Board() {
         tiles = new Tile[15][15];
@@ -49,10 +50,10 @@ public class Board {
         for (int i = 0; i < wordTiles.length; i++) {
             int r = vertical ? row + i : row;
             int c = vertical ? col : col + i;
-            if (tiles[r][c] != null && wordTiles[i] != null && tiles[r][c] != wordTiles[i]) {//TODO:i have a problem here with the null 
+            if (tiles[r][c] != null && wordTiles[i] != null && tiles[r][c] != wordTiles[i]) {
                 return false;
             }
-            if (tiles[r][c] != null && wordTiles[i] == null){//TODO:i add to change '_' for what is in this place.
+            if (tiles[r][c] != null && wordTiles[i] == null){//add this to make insted of '_' the word that need to be there.
                 wordTiles[i] = tiles[r][c];
             }
         }
@@ -86,91 +87,188 @@ public class Board {
     }
     
     public boolean dictionaryLegal(Word word) {
-        // TODO: Implement dictionary check
+        // TODO: For now its need to be true.
         return true;
     }
     
     public ArrayList<Word> getWords(Word word) {
-        ArrayList<Word> words = new ArrayList<>();
-        words.add(word);
-        
+        ArrayList<Word> newWords = new ArrayList<>();
+        newWords.add(word);
+        int down = 0;
+        int up = 0;
+        int right = 0;
+        int left = 0;
+        int r = 0;
+        int j = 0;
+        int count = 0;
+        int index;
+        int c;
+        int newR = 0;
+        int newC = 0;
+        boolean next = false;
+
         int row = word.getRow();
         int col = word.getCol();
         boolean vertical = word.isVertical();
-        Tile[] wordTiles = word.getTiles();
         
-        // Check for new words formed vertically
-        if (!vertical) {
-            for (int i = 0; i < wordTiles.length; i++) {
-                if (wordTiles[i] != null) {
-                    Word newWord = getVerticalWord(row, col + i);
-                    if (newWord != null) {
-                        words.add(newWord);
-                    }
-                }
-            }
-        }
-        
-        // Check for new words formed horizontally
         if (vertical) {
-            for (int i = 0; i < wordTiles.length; i++) {
-                if (wordTiles[i] != null) {
-                    Word newWord = getHorizontalWord(row + i, col);
-                    if (newWord != null) {
-                        words.add(newWord);
+            for (Word w : newWords) {
+
+                for (index = 0, r = row; index < w.getTiles().length && r < row + w.getTiles().length; index++, r++) {
+                    if (w.getTiles()[index] == null && this.tiles[r][col] != null) {
+                        next = true;
+                        j = r + 1;
+                        break;
+                        
                     }
                 }
             }
+            index = 0;
+            if (!next) {
+                j = row;
+            }
+            while (this.tiles[j][col] != null && j < 15) {
+
+                if (this.tiles[j][col + 1] != null && col + 1 < 15) {
+                    right = col;
+                    while (this.tiles[j][right] != null && right < 15) {
+                        
+                        if (tiles[j][right + 1] == null) {
+                            count++;
+                            break;
+                        }
+                        right++;
+                        count++;
+
+                    }
+                }
+                if (this.tiles[j][col - 1] != null && col - 1 >= 0) {
+                    if (right != 0) {
+                        left = col - 1;
+                    } else {
+                        left = col;
+                    }
+                    while (this.tiles[j][left] != null && left >= 0) {
+
+                        if (tiles[j][left - 1] == null) {
+                            count++;
+                            break;
+                        }
+                        count++;
+                        left--;
+                    }
+                }
+
+                if (count > 1) {
+                    if (left != 0) {
+                        newC = left;
+                    } else {
+                        newC = col;
+                    }
+                    c = newC;
+                    Tile[] t = new Tile[count];
+                    for (int i = 0; i < count && newC < c + count; i++) {
+                        t[i] = this.tiles[j][newC];
+                        newC++;
+                    }
+                    Word newWord = new Word(t, j, c, false);
+                    if (newWord != null) {
+                        newWords.add(newWord);
+                    }
+
+                }
+
+                j++;
+                count = 0;
+                left = 0;
+                right = 0;
+            }
+
         }
-        
-        return words;
+        if (!vertical) {
+           
+
+            for (Word w : newWords) {
+
+                for (index = 0, c = col; index < w.getTiles().length && c < col + w.getTiles().length; index++, c++) {
+                    if (w.getTiles()[index] == null && this.tiles[row][c] != null) {
+                        next = true;
+                        j = c + 1;
+                        break;
+                       
+                    }
+                }
+
+            }
+
+            index = 0;
+            if (!next) {
+                j = col;
+            }
+
+            while (this.tiles[row][j] != null && j < 15) {
+
+                if (this.tiles[row + 1][j] != null && row + 1 < 15) {
+                    down = row;
+                    while (this.tiles[down][j] != null && down < 15) {
+                        // count++;
+                        if (tiles[down + 1][j] == null) {
+                            count++;
+                            break;
+                        }
+                        down++;
+                        count++;
+
+                    }
+                }
+                if (this.tiles[row - 1][j] != null && row - 1 >= 0) {
+                    if (down != 0) {
+                        up = row - 1;
+                    } else {
+                        up = row;
+                    }
+                    while (this.tiles[up][j] != null && up >= 0) {
+
+                        if (tiles[up - 1][j] == null) {
+                            count++;
+                            break;
+                        }
+                        count++;
+                        up--;
+                    }
+                }
+
+                if (count > 1) {
+                    if (up != 0) {
+                        newR = up;
+                    } else {
+                        newR = row;
+                    }
+                    r = newR;
+                    Tile[] t = new Tile[count];
+                    for (int i = 0; i < count && newR < r + count; i++) {
+                        t[i] = this.tiles[newR][j];
+                        newR++;
+                    }
+                    Word newWord = new Word(t, r, j, true);
+                    if (newWord != null) {
+                        newWords.add(newWord);
+                    }
+
+                }
+
+                j++;
+                count = 0;
+                up = 0;
+                down = 0;
+            }
+
+        }
+
+        return newWords;
+
     }
-    
-    private Word getVerticalWord(int row, int col) {
-        int startRow = row;
-        while (startRow > 0 && tiles[startRow - 1][col] != null) {
-            startRow--;
-        }
-        
-        int endRow = row;
-        while (endRow < 14 && tiles[endRow + 1][col] != null) {
-            endRow++;
-        }
-        
-        if (startRow == endRow) {
-            return null;
-        }
-        
-        Tile[] wordTiles = new Tile[endRow - startRow + 1];
-        for (int i = startRow; i <= endRow; i++) {
-            wordTiles[i - startRow] = tiles[i][col];
-        }
-        
-        return new Word(wordTiles, startRow, col, true);
-    }
-    
-    private Word getHorizontalWord(int row, int col) {
-        int startCol = col;
-        while (startCol > 0 && tiles[row][startCol - 1] != null) {
-            startCol--;
-        }
-        
-        int endCol = col;
-        while (endCol < 14 && tiles[row][endCol + 1] != null) {
-            endCol++;
-        }
-        
-        if (startCol == endCol) {
-            return null;
-        }
-        
-        Tile[] wordTiles = new Tile[endCol - startCol + 1];
-        for (int i = startCol; i <= endCol; i++) {
-            wordTiles[i - startCol] = tiles[row][i];
-        }
-        
-        return new Word(wordTiles, row, startCol, false);
-    }
+
     
     public int getScore(Word word) {
         int score = 0;
@@ -188,7 +286,7 @@ public class Board {
             
             int letterScore = 0;
             int letterMultiplier = 1;
-            if(wordTiles[i] != null)//TODO:can delete this if statment
+            if(wordTiles[i] != null)//can delete this if statment , for now its work so i keep it.
             {
                 letterScore = wordTiles[i].score;
             }
@@ -220,14 +318,14 @@ public class Board {
     private boolean isDoubleLetterScore(int row, int col) {
         return (row == 0 || row == 14) && (col == 3 || col == 11)
                 || (row == 2 || row == 12) && (col == 6 || col == 8)
-                || (row == 3 || row == 11) && (col == 0 || col == 14 || col == 7)//TODO:add col 7.
-                || (row == 6 || row == 8) && (col == 2 || col == 12 || col == 6 || col == 8)//TODO:add col 6 , 8.
+                || (row == 3 || row == 11) && (col == 0 || col == 14 || col == 7)
+                || (row == 6 || row == 8) && (col == 2 || col == 12 || col == 6 || col == 8)
                 || (row == 7) && (col == 3 || col == 11);
     }
     
     private boolean isTripleLetterScore(int row, int col) {
         return (row == 1 || row == 13) && (col == 5 || col == 9)
-                || (row == 5 || row == 9) && (col == 1 || col == 13 || col == 5 || col == 9);//TODO:add col 5,9
+                || (row == 5 || row == 9) && (col == 1 || col == 13 || col == 5 || col == 9);
     }
     
     private boolean isDoubleWordScore(int row, int col) {
@@ -246,7 +344,7 @@ public class Board {
     
     private boolean isTripleWordScore(int row, int col) {
         return (row == 0 || row == 14) && (col == 0 || col == 14)
-                || (row == 7) && (col == 0 || col == 14);//TODO:need to be in row 7 , col 0 and 14
+                || (row == 7) && (col == 0 || col == 14);
     }
     
     public int tryPlaceWord(Word word) {
@@ -267,6 +365,7 @@ public class Board {
             score += getScore(newWord);
             placeWord(newWord);
         }
+        newWords.clear();
         return score;
     }
 
