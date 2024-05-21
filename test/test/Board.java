5016f5ad3c844eb10f4da5871ab7be9440ae3a21
Roi023023 <137299805+Roi023023@users.noreply.github.com>
2024-05-21 22:17:04 +0300

    change to hash map - because error in my calculate of bonus

--- test/test/Board.java
+++ test/test/Board.java
@@ -1,96 +1,177 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+
 
 public class Board {
-    private static Board board = null;
-    private Tile[][] tiles;
+
     boolean isStarUsed = true;
-    
+    private static Board board = null;
+    public final Tile[][] tiles;
+    private static final int ROW = 15;
+    private static final int COL = 15;
+   
+    enum BonusType {//new type of bonus
+        STAR_MIDDLE,
+        DOUBLE_LETTER,
+        TRIPLE_LETTER,
+        DOUBLE_WORD,
+        TRIPLE_WORD
+    }
+
+    private Map<String, BonusType> specialSlot;
 
     private Board() {
-        tiles = new Tile[15][15];
+        this.tiles = new Tile[15][15];
+        specialSlot = new HashMap<>();
+        initSpecialSlot();
     }
-    
+
+    private void initSpecialSlot() {
+        specialSlot.put("1,1", BonusType.DOUBLE_WORD);
+        specialSlot.put("1,13", BonusType.DOUBLE_WORD);
+        specialSlot.put("2,2", BonusType.DOUBLE_WORD);
+        specialSlot.put("2,12", BonusType.DOUBLE_WORD);
+        specialSlot.put("3,3", BonusType.DOUBLE_WORD);
+        specialSlot.put("3,11", BonusType.DOUBLE_WORD);
+        specialSlot.put("4,4", BonusType.DOUBLE_WORD);
+        specialSlot.put("4,10", BonusType.DOUBLE_WORD);
+        specialSlot.put("10,4", BonusType.DOUBLE_WORD);
+        specialSlot.put("10,10", BonusType.DOUBLE_WORD);
+        specialSlot.put("11,3", BonusType.DOUBLE_WORD);
+        specialSlot.put("11,11", BonusType.DOUBLE_WORD);
+        specialSlot.put("12,2", BonusType.DOUBLE_WORD);
+        specialSlot.put("12,12", BonusType.DOUBLE_WORD);
+        specialSlot.put("13,1", BonusType.DOUBLE_WORD);
+        specialSlot.put("13,13", BonusType.DOUBLE_WORD);
+
+        specialSlot.put("7,7", BonusType.STAR_MIDDLE);//star is also worth rtiple word , but only once.
+        specialSlot.put("0,0", BonusType.TRIPLE_WORD);
+        specialSlot.put("0,7", BonusType.TRIPLE_WORD);
+        specialSlot.put("0,14", BonusType.TRIPLE_WORD);
+        specialSlot.put("7,0", BonusType.TRIPLE_WORD);
+        specialSlot.put("7,14", BonusType.TRIPLE_WORD);
+        specialSlot.put("14,0", BonusType.TRIPLE_WORD);
+        specialSlot.put("14,7", BonusType.TRIPLE_WORD);
+        specialSlot.put("14,14", BonusType.TRIPLE_WORD);
+
+        specialSlot.put("0,3", BonusType.DOUBLE_LETTER);
+        specialSlot.put("0,11", BonusType.DOUBLE_LETTER);
+        specialSlot.put("2,6", BonusType.DOUBLE_LETTER);
+        specialSlot.put("2,8", BonusType.DOUBLE_LETTER);
+        specialSlot.put("3,0", BonusType.DOUBLE_LETTER);
+        specialSlot.put("3,7", BonusType.DOUBLE_LETTER);
+        specialSlot.put("3,14", BonusType.DOUBLE_LETTER);
+        specialSlot.put("6,2", BonusType.DOUBLE_LETTER);
+        specialSlot.put("6,6", BonusType.DOUBLE_LETTER);
+        specialSlot.put("6,8", BonusType.DOUBLE_LETTER);
+        specialSlot.put("6,12", BonusType.DOUBLE_LETTER);
+        specialSlot.put("7,3", BonusType.DOUBLE_LETTER);
+        specialSlot.put("7,11", BonusType.DOUBLE_LETTER);
+        specialSlot.put("8,2", BonusType.DOUBLE_LETTER);
+        specialSlot.put("8,6", BonusType.DOUBLE_LETTER);
+        specialSlot.put("8,8", BonusType.DOUBLE_LETTER);
+        specialSlot.put("8,12", BonusType.DOUBLE_LETTER);
+        specialSlot.put("11,0", BonusType.DOUBLE_LETTER);
+        specialSlot.put("11,7", BonusType.DOUBLE_LETTER);
+        specialSlot.put("11,14", BonusType.DOUBLE_LETTER);
+        specialSlot.put("12,6", BonusType.DOUBLE_LETTER);
+        specialSlot.put("12,8", BonusType.DOUBLE_LETTER);
+        specialSlot.put("14,3", BonusType.DOUBLE_LETTER);
+        specialSlot.put("14,11", BonusType.DOUBLE_LETTER);
+
+        specialSlot.put("1,5", BonusType.TRIPLE_LETTER);
+        specialSlot.put("1,9", BonusType.TRIPLE_LETTER);
+        specialSlot.put("5,1", BonusType.TRIPLE_LETTER);
+        specialSlot.put("5,5", BonusType.TRIPLE_LETTER);
+        specialSlot.put("5,9", BonusType.TRIPLE_LETTER);
+        specialSlot.put("5,13", BonusType.TRIPLE_LETTER);
+        specialSlot.put("9,1", BonusType.TRIPLE_LETTER);
+        specialSlot.put("9,5", BonusType.TRIPLE_LETTER);
+        specialSlot.put("9,9", BonusType.TRIPLE_LETTER);
+        specialSlot.put("9,13", BonusType.TRIPLE_LETTER);
+        specialSlot.put("13,5", BonusType.TRIPLE_LETTER);
+        specialSlot.put("13,9", BonusType.TRIPLE_LETTER);
+        
+
+    }
+
     public static Board getBoard() {
-        if (board == null) {
+        if (board == null) {//check if we already have a board..
             board = new Board();
         }
         return board;
     }
-    
+
     public Tile[][] getTiles() {
         return Arrays.stream(tiles).map(Tile[]::clone).toArray(Tile[][]::new);
     }
-    
-    public boolean boardLegal(Word word) {
-        int row = word.getRow();
-        int col = word.getCol();
-        boolean vertical = word.isVertical();
-        Tile[] wordTiles = word.getTiles();
-        
-        // Check if the word is within the board boundaries
-        if (row < 0 || row >= 15 || col < 0 || col >= 15) {
+
+    private boolean VerticalWord(Word w) {
+        int row = w.getRow();
+        int col = w.getCol();
+        int length = w.getTiles().length;
+
+        if (row + length >= 15) {//go out of the board.
             return false;
         }
-        
-        // Check if the word fits on the board
-        if (vertical) {
-            if (row + wordTiles.length > 15) {
-                return false;
+        if (this.tiles[7][7] == null) {
+            if (col == 7 && row <= 7 && row + length >= 7) {
+                return true;
             }
         } else {
-            if (col + wordTiles.length > 15) {
-                return false;
-            }
-        }
-        
-        // Check if the word overlaps with existing tiles
-        for (int i = 0; i < wordTiles.length; i++) {
-            int r = vertical ? row + i : row;
-            int c = vertical ? col : col + i;
-            if (tiles[r][c] != null && wordTiles[i] != null && tiles[r][c] != wordTiles[i]) {
-                return false;
-            }
-            if (tiles[r][c] != null && wordTiles[i] == null){//add this to make insted of '_' the word that need to be there.
-                wordTiles[i] = tiles[r][c];
-            }
-        }
-        
-        // Check if the word is connected to an existing tile or the star in the center
-        boolean connected = false;
-        for (int i = 0; i < wordTiles.length; i++) {
-            int r = vertical ? row + i : row;
-            int c = vertical ? col : col + i;
-            if (hasTileAdjacent(r, c) || (r == 7 && c == 7)) {
-                connected = true;
-                break;
-            }
-        }
-        
-        return connected;
-    }
-    
-    private boolean hasTileAdjacent(int row, int col) {
-        // Check if there is a tile in any adjacent position
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                int r = row + i;
-                int c = col + j;
-                if (r >= 0 && r < 15 && c >= 0 && c < 15 && tiles[r][c] != null) {
+            for (int i = 0; i < length; i++) {
+                if ((tiles[row + i][col - 1] != null && col - 1 >= 0) || (tiles[row + i][col + 1] != null && col + 1 < COL)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    
+
+    private boolean NotVerticalWord(Word w) {
+        int row = w.getRow();
+        int col = w.getCol();
+        int length = w.getTiles().length;
+
+        if (col + length >= 15) {//go out of board.
+            return false;
+        }
+        if (this.tiles[7][7] == null) {
+            if (row == 7 && col <= 7 && col + length >= 7) {
+                return true;
+            }
+        } else {
+            for (int i = 0; i < length; i++) {
+                if ((tiles[row - 1][col + i] != null && row - 1 >= 0) || (tiles[row + 1][col + i] != null && row + 1 < ROW)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean boardLegal(Word w0) {
+        int row = w0.getRow();
+        int col = w0.getCol();
+        if (row < 0 || row >= ROW || col < 0 || col >= COL) {
+            return false;
+        }
+        if (w0.isVertical()) {
+            if (VerticalWord(w0)) return true;
+        }
+        else {
+            if (NotVerticalWord(w0)) return true;
+        }
+        return false;
+    }
+
+   
     public boolean dictionaryLegal(Word word) {
-        // TODO: For now its need to be true.
+        // TODO: For now its need to be true always.
         return true;
     }
-    
+
     public ArrayList<Word> getWords(Word word) {
         ArrayList<Word> words = new ArrayList<>();
         words.add(word);
@@ -173,108 +254,115 @@ public class Board {
         return new Word(wordTiles, row, startCol, false);
     }
 
-    
+    private BonusType getBonus(int row, int col) {
+        String key = row + "," + col;
+        if (specialSlot.containsKey(key)) {
+            return specialSlot.get(key);
+        }
+        return null;
+    }
+
     public int getScore(Word word) {
-        int score = 0;
-        int wordMultiplier = 1;
-        int[] wordScores = new int[word.getTiles().length];
-        
         int row = word.getRow();
         int col = word.getCol();
-        boolean vertical = word.isVertical();
-        Tile[] wordTiles = word.getTiles();
-        
-        for (int i = 0; i < wordTiles.length; i++) {
-            int r = vertical ? row + i : row;
-            int c = vertical ? col : col + i;
-            
-            int letterScore = 0;
-            int letterMultiplier = 1;
-            if(wordTiles[i] != null)//can delete this if statment , for now its work so i keep it.
-            {
-                letterScore = wordTiles[i].score;
-            }
+        int lenght = word.getTiles().length;
+        int score = 0;
+        int wordMultiplier = 1;
+        int letterScore = 0;
+        boolean isVertical = word.isVertical();
+        if (!isVertical) {
+            for (int i = 0; i < lenght; i++) {
+                if (word.getTiles()[i] == null) {
 
-            // Check for bonus squares
-            if (tiles[r][c] != null) {
-                if (isDoubleLetterScore(r, c)) {
-                    letterMultiplier = 2;
-                } else if (isTripleLetterScore(r, c)) {
-                    letterMultiplier = 3;
-                } else if (isDoubleWordScore(r, c)) {
-                    wordMultiplier *= 2;
-                } else if (isTripleWordScore(r, c)) {
-                    wordMultiplier *= 3;
+                    letterScore = this.tiles[row][col + i].getScore();
+                } else {
+                    letterScore = word.getTiles()[i].getScore();
                 }
+                
+                    BonusType bonus = getBonus(row, col + i);
+                    if (bonus != null) {
+                        switch (bonus) {
+                            case DOUBLE_LETTER:
+                                letterScore *= 2;
+                                break;
+                            case TRIPLE_LETTER:
+                                letterScore *= 3;
+                                break;
+                            case STAR_MIDDLE:
+                                wordMultiplier *= 2;
+                                specialSlot.remove("7,7");//only the first one is get the special.
+                                break;
+                            case DOUBLE_WORD:
+                                wordMultiplier *= 2;
+                                break;
+                            case TRIPLE_WORD:
+                                wordMultiplier *= 3;
+                                break;
+                        }
+                    }
+                score += letterScore;
             }
-            
-            wordScores[i] = letterScore * letterMultiplier;
+            score *= wordMultiplier;
+
+        } else {//if its vertical
+            for (int i = 0; i < lenght; i++) {
+                if (word.getTiles()[i] == null) {
+                    letterScore = this.tiles[row + i][col].getScore();
+                } else {
+                    letterScore = word.getTiles()[i].getScore();
+                }
+                    BonusType bonus = getBonus(row + i, col);
+                    if (bonus != null) {
+                        switch (bonus) {
+                            case DOUBLE_LETTER:
+                                letterScore *= 2;
+                                break;
+                            case TRIPLE_LETTER:
+                                letterScore *= 3;
+                                break;
+                            case STAR_MIDDLE:
+                                specialSlot.remove("7,7");//only the first one is get the special.
+                                wordMultiplier *= 2;
+                                break;
+                            case DOUBLE_WORD:
+                                wordMultiplier *= 2;
+                                break;
+                            case TRIPLE_WORD:
+                                wordMultiplier *= 3;
+                                break;
+                        }
+                    }
+                score += letterScore;
+            }
+            score *= wordMultiplier;
         }
-        
-        for (int wordScore : wordScores) {
-            score += wordScore;
-        }
-        
-        score *= wordMultiplier;
         return score;
     }
-    
-    private boolean isDoubleLetterScore(int row, int col) {
-        return (row == 0 || row == 14) && (col == 3 || col == 11)
-                || (row == 2 || row == 12) && (col == 6 || col == 8)
-                || (row == 3 || row == 11) && (col == 0 || col == 14 || col == 7)
-                || (row == 6 || row == 8) && (col == 2 || col == 12 || col == 6 || col == 8)
-                || (row == 7) && (col == 3 || col == 11);
-    }
-    
-    private boolean isTripleLetterScore(int row, int col) {
-        return (row == 1 || row == 13) && (col == 5 || col == 9)
-                || (row == 5 || row == 9) && (col == 1 || col == 13 || col == 5 || col == 9);
-    }
-    
-    private boolean isDoubleWordScore(int row, int col) {
-        
-        if (isStarUsed && row == 7 && col == 7) {
-            isStarUsed = false;
-            return true;
-        }
-        
-        return (row == 1 || row == 13) && (col == 1 || col == 13)
-        || (row == 2 || row == 12) && (col == 2 || col == 12)
-        || (row == 3 || row == 11) && (col == 3 || col == 11)
-        || (row == 4 || row == 10) && (col == 4 || col == 10);
-        
-    }
-    
-    private boolean isTripleWordScore(int row, int col) {
-        return (row == 0 || row == 14) && (col == 0 || col == 14)
-                || (row == 7) && (col == 0 || col == 14);
-    }
-    
+
     public int tryPlaceWord(Word word) {
+
         if (!boardLegal(word)) {
             return 0;
         }
+        
         placeWord(word);
-        int score = 0;
+        int totalScore = 0;
 
         ArrayList<Word> newWords = getWords(word);
-
         for (Word newWord : newWords) {
-            score += getScore(newWord);
-            
+            totalScore += getScore(newWord);
         }
 
         for (Word newWord : newWords) {
+
             if (!dictionaryLegal(newWord)) {
                 return 0;
             }
+           
         }
-
-        return score;
+        return totalScore;
     }
 
-    
     private void placeWord(Word word) {
         int row = word.getRow();
         int col = word.getCol();
@@ -289,4 +377,5 @@ public class Board {
             }
         }
     }
+
 }
